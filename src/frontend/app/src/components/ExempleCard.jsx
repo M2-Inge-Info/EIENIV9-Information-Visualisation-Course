6cc5b6c3839beb6f5c0aec8f17450080a4753130
Jojo <58971708+JojoGit01@users.noreply.github.com>
2023-09-25 11:06:04 +0200

    delete react app

--- src/frontend/app/src/components/ExempleCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ExempleCard = () => {
-  return (
-    <div>ExempleCard</div>
-  )
-}
-
-export default ExempleCard
--- src/frontend/app/src/pages/Vizu1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Vizu1 = () => {
-  return (
-    <div>Vizu1</div>
-  )
-}
-
-export default Vizu1
