aa20c7c25924f764a9448139af692539a1dff231
cainmosn1 <117351027+InsomniacGit@users.noreply.github.com>
2023-10-02 22:41:19 +0200

    Ajout de Query utiles
    Ajout de quelque Query permettant de mieux comprendre la structure du dataset

--- getArtists.py
+++ getArtists.py
@@ -1,6 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
-from rdflib import Graph, Literal, URIRef, RDF
-from rdflib.namespace import FOAF
+from rdflib import Graph, Literal, URIRef
+from rdflib.namespace import RDF, RDFS, FOAF
+import csv
 
 # Définissez l'URL du point de terminaison SPARQL
 sparql_endpoint = "http://wasabi.inria.fr/sparql"
@@ -14,31 +15,45 @@ PREFIX wsb: <http://ns.inria.fr/wasabi/ontology/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX schema: <http://schema.org/>
 
-SELECT DISTINCT ?artist ?artistName ?artistType
+SELECT DISTINCT ?artist ?artistName ?artistType ?idArtistMusicBrainz ?idArtistDeezer ?idArtistDiscogs ?name ?genres ?labels ?recordLabel ?gender ?disambiguation ?endAreaId ?endAreaName ?endAreaDisambiguation ?ended ?begin ?end ?city ?country ?locationInfo ?deezerFans ?picture ?associatedMusicalArtist ?dbpAbstract ?dbpGenre ?subject ?rdf ?abstract ?nameVariations ?urls ?nameVariationsFold ?animuxPath ?animuxPathAmbiguous ?nameAccentFold
 WHERE {
-  {
-    ?artist a wsb:Artist_Person ;
-            foaf:name ?artistName .
-    BIND("Solo Music Artist" AS ?artistType)
-  }
-  UNION
-  {
-    ?artist a wsb:Artist_Group ;
-            foaf:name ?artistName .
-    BIND("Music Group" AS ?artistType)
-  }
-  UNION
-  {
-    ?artist a wsb:Choir ;
-            foaf:name ?artistName .
-    BIND("Choir" AS ?artistType)
-  }
-  UNION
-  {
-    ?artist a wsb:Orchestra ;
-            foaf:name ?artistName .
-    BIND("Orchestra" AS ?artistType)
-  }
+  ?artist a ?artistType .
+  ?artist foaf:name ?artistName .
+  OPTIONAL { ?artist wsb:id_artist_musicbrainz ?idArtistMusicBrainz }
+  OPTIONAL { ?artist wsb:id_artist_deezer ?idArtistDeezer }
+  OPTIONAL { ?artist wsb:id_artist_discogs ?idArtistDiscogs }
+  OPTIONAL { ?artist wsb:name ?name }
+  OPTIONAL { ?artist wsb:genres ?genres }
+  OPTIONAL { ?artist wsb:labels ?labels }
+  OPTIONAL { ?artist wsb:recordLabel ?recordLabel }
+  OPTIONAL { ?artist wsb:gender ?gender }
+  OPTIONAL { ?artist wsb:disambiguation ?disambiguation }
+  OPTIONAL { ?artist wsb:endArea ?endAreaId .
+             ?endAreaId wsb:name ?endAreaName .
+             ?endAreaId wsb:disambiguation ?endAreaDisambiguation }
+  OPTIONAL { ?artist wsb:lifeSpan ?lifeSpan .
+             ?lifeSpan wsb:ended ?ended .
+             ?lifeSpan wsb:begin ?begin .
+             ?lifeSpan wsb:end ?end }
+  OPTIONAL { ?artist wsb:location ?location .
+             ?location wsb:id_city_musicbrainz ?cityId .
+             ?cityId wsb:city ?city .
+             ?cityId wsb:country ?country }
+  OPTIONAL { ?artist wsb:locationInfo ?locationInfo }
+  OPTIONAL { ?artist wsb:deezerFans ?deezerFans }
+  OPTIONAL { ?artist wsb:picture ?picture }
+  OPTIONAL { ?artist wsb:associatedMusicalArtist ?associatedMusicalArtist }
+  OPTIONAL { ?artist wsb:dbpAbstract ?dbpAbstract }
+  OPTIONAL { ?artist wsb:dbpGenre ?dbpGenre }
+  OPTIONAL { ?artist wsb:subject ?subject }
+  OPTIONAL { ?artist wsb:rdf ?rdf }
+  OPTIONAL { ?artist wsb:abstract ?abstract }
+  OPTIONAL { ?artist wsb:nameVariations ?nameVariations }
+  OPTIONAL { ?artist wsb:urls ?urls }
+  OPTIONAL { ?artist wsb:nameVariationsFold ?nameVariationsFold }
+  OPTIONAL { ?artist wsb:animuxPath ?animuxPath }
+  OPTIONAL { ?artist wsb:animuxPathAmbiguous ?animuxPathAmbiguous }
+  OPTIONAL { ?artist wsb:nameAccentFold ?nameAccentFold }
 }
 ORDER BY ?artistName
 """
@@ -50,18 +65,23 @@ sparql.setReturnFormat(JSON)
 sparql.setQuery(query)
 results = sparql.query().convert()
 
-# Créez un graph RDF
+# Créez un graph RDF pour stocker les données
 g = Graph()
 
-# Parcourez les résultats et ajoutez-les au graph RDF avec le type d'artiste
+# Parcourez les résultats et ajoutez chaque triple au graph RDF
 for result in results["results"]["bindings"]:
-    artist_uri = URIRef(result["artist"]["value"])
-    artist_name = Literal(result["artistName"]["value"])
-    artist_type = Literal(result["artistType"]["value"])
-    g.add((artist_uri, RDF.type, artist_type))
-    g.add((artist_uri, FOAF.name, artist_name))
+    artist = URIRef(result["artist"]["value"])
+    artistName = Literal(result["artistName"]["value"])
+    artistType = URIRef(result["artistType"]["value"])
+    # Ajoutez d'autres variables ici...
 
-# Enregistrez le graph RDF dans un fichier TTL
-g.serialize(destination='src/data/artist_data_with_types.ttl', format='turtle')
+    # Ajoutez les triples au graph
+    g.add((artist, RDF.type, artistType))
+    g.add((artist, FOAF.name, artistName))
+    # Ajoutez d'autres triples ici...
 
-print("Les données avec les types d'artistes ont été enregistrées dans artist_data_with_types.ttl")
+# Enregistrez le graph RDF au format TTL
+output_file = "src/data/artist_data_with_types.ttl"
+g.serialize(destination=output_file, format="turtle")
+
+print(f"Les données ont été enregistrées dans {output_file}")
