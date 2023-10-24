838c05e40a237bd5e7da616c24f7b352365b6984
Niteuk <78158258+Niteuk@users.noreply.github.com>
2023-10-24 10:53:58 +0200

    Modification du traitement de données principale
    Intervalle d'années plus courte et trie en fonction de l'année sur le CSV

--- quentinData/getPopulationAlbum.py
+++ quentinData/getPopulationAlbum.py
@@ -5,15 +5,20 @@ df = pd.read_csv("src\data\quentin_data_artists.csv")
 
 # Créer une nouvelle colonne pour la tranche d'âge en fonction de la date de sortie
 df['année'] = pd.cut(pd.to_datetime(df['Date de sortie']).dt.year, 
-                              bins=[0, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020, 9999], 
-                              labels=['<1950', '1950-1960', '1960-1970', '1970-1980', '1980-1990', '1990-2000', '2000-2010', '2010-2020', '>2020'])
+                     bins=[0, 1970, 1980, 1990, 2000, 2010, 2020, 9999], 
+                     labels=['<1970', '1970-1980', '1980-1990', '1990-2000', '2000-2010', '2010-2020', '>2020'])
 
 # Regrouper et compter le nombre d'albums par genre et tranche d'âge
 result_df = df.groupby(['Genre', 'année']).size().reset_index(name='Nombre d\'albums')
 result_df = result_df.rename(columns={'Titre Album': 'Nombre d\'albums'})
 
-##### Filtrer les lignes où "Nombre d'albums" est supérieur à 0 #####
-## result_df = result_df.query('`Nombre d\'albums` > 0')
+# Réorganiser les tranches d'âge dans l'ordre souhaité
+result_df['année'] = pd.Categorical(result_df['année'], 
+                                    categories=['<1970', '1970-1980', '1980-1990', '1990-2000', '2000-2010', '2010-2020', '>2020'], 
+                                    ordered=True)
+
+# Trier le DataFrame résultant en fonction de l'année
+result_df = result_df.sort_values(by='année')
 
 # Sauvegarder le DataFrame résultant dans un fichier CSV
 result_df.to_csv("src\data\quentin_population_album.csv", index=False)
